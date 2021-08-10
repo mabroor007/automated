f87717ae03c1dfb4708d8dc05f66457ab950c461
mabroor007 <amabroor007@gmail.com>
2021-08-11 02:28:51 +0500

    p fixed

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Do not Edit now <code>src/App.tsx</code> and save to reload.
+          Edit now <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
