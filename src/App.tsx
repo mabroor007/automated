1c84617ff11885676297136e94ea6d9b0150d875
mabroor007 <amabroor007@gmail.com>
2021-08-11 02:00:11 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Mabroor Ahmad
+          Learn React with Mabroor Ahmad, Lets go!
         </a>
       </header>
     </div>
